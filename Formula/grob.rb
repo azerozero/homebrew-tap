79924aa506a3a0dd355520af941554b23767eae6
clement <cliard@a00.fr>
2026-03-21 19:52:17 +0100

    feat: add grob formula v0.24.4

---
+++ Formula/grob.rb
@@ -0,0 +1,34 @@
+class Grob < Formula
+  desc "High-performance LLM routing proxy with built-in DLP"
+  homepage "https://github.com/azerozero/grob"
+  version "0.24.4"
+  license "AGPL-3.0-only"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/azerozero/grob/releases/download/v#{version}/grob-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "62738116d68d37544b5c667ec75307810c1f914fac08a4f3993d4525a8bf3b63"
+    else
+      url "https://github.com/azerozero/grob/releases/download/v#{version}/grob-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "f43d237bb6e11f779842cf5da0fd5dc890bc0acdc19bbd85bf5b2841998596ac"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/azerozero/grob/releases/download/v#{version}/grob-v#{version}-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "a5ba77d7060ab532ca2ec9c18f28d736c91e00b21dc4c2888f0a90d98bc39ab7"
+    else
+      url "https://github.com/azerozero/grob/releases/download/v#{version}/grob-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "643c9b1beafff779cf8dbb2f50fc3f1535b825a7e2994ab97bc2357015ddb211"
+    end
+  end
+
+  def install
+    bin.install "grob"
+  end
+
+  test do
+    assert_match "grob #{version}", shell_output("#{bin}/grob --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Homebrew Tap for Grob
+
+LLM routing proxy with built-in DLP. See [grob](https://github.com/azerozero/grob).
+
+## Install
+
+```bash
+brew install azerozero/tap/grob
+```
+
+## Update
+
+```bash
+brew upgrade grob
+```
